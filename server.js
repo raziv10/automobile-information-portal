08078d1f0a99c36a8f1a675ae1f3fc7d2f717c13
Rajiv <razivluitel@gmail.com>
2020-04-13 18:02:03 +0545

    New App

--- server.js
+++ server.js
@@ -1,11 +1,17 @@
+
 //Install express server
 const express = require('express');
 const path = require('path');
+
 const app = express();
+
 // Serve only the static files form the dist directory
 app.use(express.static(__dirname + '/dist/automobile-information-portal'));
-app.get('*', function(req,res) {
-  res.sendFile(path.join(__dirname + '/dist/automobile-information-portal/src/index.html'));
+
+app.get('/*', function(req,res) {
+
+  res.sendFile(path.join(__dirname+'/dist/automobile-information-portal/index.html'));
 });
+
 // Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
