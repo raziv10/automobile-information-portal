e57221c17669a9a027dae11de9acb869ac7a9523
Rajiv <razivluitel@gmail.com>
2020-04-13 17:34:20 +0545

    second app

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const app = express();
 // Serve only the static files form the dist directory
 app.use(express.static(__dirname + '/dist/automobile-information-portal'));
 app.get('*', function(req,res) {
-  res.sendFile(path.join(__dirname + '/dist/automobile-information-portal/index.html'));
+  res.sendFile(path.join(__dirname + '/dist/automobile-information-portal/src/index.html'));
 });
 // Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
